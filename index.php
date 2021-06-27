18865eca9d7cecd6ddf1557cb190d6bbd32b14cd
Matej Kuráň <matejkuran@Matejs-MacBook-Pro.local>
2021-06-27 21:04:36 +0200

    zmena na html

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $request = $_SERVER['REQUEST_URI'];
 if (preg_match("/\/json*/", $request)) {
     require __DIR__ . '/api.php';
 } else if ($request == '' || $request == '/') {
-    require __DIR__ . '/views/index.php';
+    require __DIR__ . '/views/index.html';
 } else{
     echo "BAAAD";
 }
