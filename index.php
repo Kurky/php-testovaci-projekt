08a742fe73ba336ac06e2a6df62e9a624a366c19
Matej Kuráň <matejkuran@Matejs-MacBook-Pro.local>
2021-06-28 10:24:14 +0200

    pridanie 404 v index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ if (preg_match("/\/json*/", $request)) {
     require __DIR__ . '/api.php';
 } else if ($request == '' || $request == '/') {
     require __DIR__ . '/views/index.html';
-} else{
-    echo "BAAAD";
+} else {
+    http_response_code(404);
+    echo "404 Not Found";
 }
